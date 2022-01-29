7b2f91348570fbad6eb09cbea8fb25333feb2770
theRoushan <roushank00978@gmail.com>
2022-01-29 18:04:18 +0530

    done q4

---
+++ positiveNegativeZero.js
@@ -0,0 +1,12 @@
+//program to check whether entered number is positive or negative or zero
+
+const num = 10;
+if (num > 0) {
+    console.log("Positive");
+}
+else if (num < 0) {
+    console.log("Negative");
+}
+else {
+    console.log("Zero");
+}
